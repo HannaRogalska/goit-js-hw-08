d95a157f9509dc9561b1b021e0c197c7083d8772
Hanna Rogalska <hanrogalska@gmail.com>
2023-03-04 12:47:40 +0100

    added task 3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+const ulRef = document.querySelector('.gallery');
+
+const getContainGallary = document.querySelector('.gallery');
+console.log(getContainGallary);
+const setNewItemsElements = galleryItems
+  .map(elem => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${elem.original}">
+    <img class="gallery__image"
+    src="${elem.preview}"
+    data-source="${elem.original}"
+    alt="${elem.description}"/>
+    </a>
+    </div>`;
+  })
+  .join('');
+console.log(setNewItemsElements);
+getContainGallary.insertAdjacentHTML('beforeend', setNewItemsElements);
+
+const lightbox = new SimpleLightbox('.gallery a', { close: true });
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import { throttle } from 'lodash';
+const formElement = document.querySelector('.feedback-form');
+console.log(formElement);
 
+formElement.addEventListener('submit', throttle(formSubmit, 500));
+window.addEventListener('load', winFunction);
+
+// Po wczytywaniu okna wykonamy zapisylanie mail oraz massage
+
+function winFunction() {
+  Array.from(formElement.elements).forEach(el => {
+    if (el.name) {
+      const prevVal = localStorage.getItem(el.name);
+      el.value = prevVal;
+    }
+  });
+}
+
+// Nasluchuje i zapisuje  localStorage
+function formSubmit(event) {
+  Array.from(formElement.elements).forEach(el => {
+    if (el.name) {
+      localStorage.setItem(el.name, el.value);
+    }
+  });
+}
